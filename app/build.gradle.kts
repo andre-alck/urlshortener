58b5031af66aff4af316a042f0bb85c709041c08
andre-alck <andrealck1@gmail.com>
2024-11-24 18:42:44 -0300

    build: add aws lambda dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,9 @@ dependencies {
 
     // This dependency is used by the application.
     implementation(libs.guava)
+
+    implementation(platform("software.amazon.awssdk:bom:2.29.20"))
+    implementation("software.amazon.awssdk:lambda")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
@@ -34,7 +37,7 @@ java {
 
 application {
     // Define the main class for the application.
-    mainClass = "org.example.App"
+    mainClass = "com.asacxyz.App"
 }
 
 tasks.named<Test>("test") {
